b08207730552dd9f8632ad46e3fb810dd9d538ec
skip mcbride <skip@angerbunny.com>
2025-07-07 09:47:58 -0700

    adding coloured lights

--- app.py
+++ app.py
@@ -23,5 +23,19 @@ def button():
     return 'Button pressed'
   return 'Button not pressed'
 
+@app.route('/red')
+def red():
+  '''This route simulates turning on a red LED.'''
+  GPIO.setup(pin_list[0], GPIO.OUT)
+  GPIO.output(pin_list[0], GPIO.HIGH)
+  return 'Red LED is ON'
+
+@app.route('/blue')
+def blue():
+  '''This route simulates turning on a red LED.'''
+  GPIO.setup(pin_list[1], GPIO.OUT)
+  GPIO.output(pin_list[1], GPIO.HIGH)
+  return 'blue LED is ON'
+
 if __name__ == '__main__':
   app.run(debug= True, host='0.0.0.0', port=5000)
