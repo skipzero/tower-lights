83d17702e47e3ffebd425be9ecaf528b3704f0dc
skip mcbride <skip@angerbunny.com>
2025-07-07 06:30:00 -0700

    commiting to move to rpi for further development

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from flask import Flask
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+pin_list = [1,2,3,4]
+BUTTON_PIN = 3
+GPIO.setup(pin_list, GPIO.IN)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  '''This is the main route of the Flask application.'''
+  return "Hello, World!"
+
+@app.route('/button')
+def button():
+  '''This route simulates a button click.'''
+  if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+    # Simulate button click action
+    return 'handle_button_click()'
